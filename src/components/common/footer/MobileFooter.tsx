161abc4433df18997d700c57adc2525a06c20bfa
Yuma Satake <satous.y@icloud.com>
2023-10-26 14:48:23 +0900

    feat: フッター追加

---
+++ src/components/common/footer/MobileFooter.tsx
@@ -0,0 +1,46 @@
+import { Box, Stack, Typography } from '@mui/material';
+import { FC } from 'react';
+import footerLogo from '@/assets/images/headfoot/footer-logo.png';
+import Image from 'next/image';
+
+export const MobileFooter: FC = () => {
+  return (
+    <Stack
+      spacing={1}
+      sx={{
+        mt: 1,
+      }}
+    >
+      <Stack alignContent="flex-start" spacing={2} sx={{ px: 2, py: 1 }}>
+        <Image
+          src={footerLogo}
+          alt="名古屋工学院専門学校"
+          style={{
+            width: '65%',
+            height: 'auto',
+          }}
+        />
+        <Box>
+          <Typography variant="body2">
+            TEL：052-681-1311 ／ FAX：052-682-3017
+            <br />
+            〒456-0031 名古屋市熱田区神宮4-7-21
+          </Typography>
+        </Box>
+      </Stack>
+      <Box
+        sx={{
+          width: '100vw',
+          color: 'white',
+          bgcolor: 'gray',
+          px: 2,
+          py: 1,
+        }}
+      >
+        <Typography variant="caption">
+          Copyright © Nagoya Kogakuin College of Technology All Rights Reserved
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
