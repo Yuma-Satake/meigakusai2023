41461c1463d888b50cc0b136e94237b38105eb8c
Yuma Satake <satous.y@icloud.com>
2023-10-27 11:22:37 +0900

    feat: MenuDrawerデザイン変更

--- src/features/Menu/MenuDrawer.tsx
+++ src/features/Menu/MenuDrawer.tsx
@@ -3,31 +3,40 @@ import { discloserWrapper } from '@/hooks/recoilWrapper/discloserWrapper';
 import { isOpenMenuState } from '@/hooks/state/isOpenMenuState';
 import { Box, Button, Drawer, IconButton, Stack, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { useRecoilState } from 'recoil';
 import CloseIcon from '@mui/icons-material/Close';
+import HomeIcon from '@mui/icons-material/Home';
+import FestivalIcon from '@mui/icons-material/Festival';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import CommuteIcon from '@mui/icons-material/Commute';
 
 type MenuItemType = {
   label: string;
   path: string;
+  icon: ReactNode;
 };
 
 const menuItem: MenuItemType[] = [
   {
     label: 'ホーム',
     path: PathEnum.HOME,
+    icon: <HomeIcon />,
   },
   {
     label: 'イベント',
     path: PathEnum.EVENTS,
+    icon: <FestivalIcon />,
   },
   {
-    label: '模擬店一覧',
+    label: '模擬店',
     path: PathEnum.BOOTH,
+    icon: <StorefrontIcon />,
   },
   {
     label: 'アクセス',
     path: PathEnum.ACCESS,
+    icon: <CommuteIcon />,
   },
 ];
 
@@ -37,24 +46,28 @@ export const MenuDrawer: FC = () => {
 
   return (
     <Drawer open={isOpenMenu} onClose={isOpenMenuToggle} anchor="right" transitionDuration={400}>
-      <Box sx={{ width: '40vw', height: '100vh' }}>
+      <Box sx={{ width: '70vw' }}>
         <Stack alignItems="flex-end">
           <IconButton onClick={isOpenMenuToggle}>
             <CloseIcon fontSize="large" />
           </IconButton>
         </Stack>
-        <Stack alignItems="flex-start" spacing={3} sx={{ p: 2 }}>
-          {menuItem.map((item) => (
-            <Button
-              key={item.path}
-              variant="outlined"
-              onClick={() => {
-                router.push(item.path);
-              }}
-            >
-              <Typography>{item.label}</Typography>
-            </Button>
-          ))}
+        <Stack spacing={2} sx={{ p: 2 }}>
+          {menuItem.map((item) => {
+            return (
+              <Button
+                key={item.label}
+                variant="contained"
+                size="large"
+                startIcon={item.icon}
+                onClick={() => {
+                  router.push(item.path);
+                }}
+              >
+                <Typography sx={{ width: '4em', textAlign: 'left' }}>{item.label}</Typography>
+              </Button>
+            );
+          })}
         </Stack>
       </Box>
     </Drawer>
