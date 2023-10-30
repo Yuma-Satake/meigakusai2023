3d36df581fed86d318b88d497bb19b30c771e500
Yuma Satake <satous.y@icloud.com>
2023-10-31 08:23:02 +0900

    feat: img読み込みに変更

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isDevMode = process.env.NEXT_PUBLIC_DEV_MODE === 'true';
+const prefixPath = isDevMode ? '' : process.env.NEXT_PUBLIC_REFIX_PATH;
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  output: 'export',
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
 };
 
 module.exports = nextConfig;
