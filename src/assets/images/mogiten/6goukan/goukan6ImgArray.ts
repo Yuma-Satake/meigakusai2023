117f836481b6b1477daec1ee21d78bae2acee4f2
Yuma Satake <satous.y@icloud.com>
2023-11-01 19:49:43 +0900

    feat: メタデータ追加

--- src/assets/images/mogiten/6goukan/goukan6ImgArray.ts
+++ src/assets/images/mogiten/6goukan/goukan6ImgArray.ts
@@ -1,5 +1,5 @@
 import { ImgItemType } from '@/types/ImgItemType';
 
 export const goukan6ImgArray: ImgItemType[] = [
-  { src: './01_618-kyoumu.jpg', alt: 'HidesCoffeeコーヒーアイスクリームcoffee' },
+  { src: './01_618-kyoumu.jpg', alt: 'HidesCoffeeコーヒーアイスクリームcoffee林秀聡' },
 ];
