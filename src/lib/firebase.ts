cef935cd4d67258ddd96cce4ee047566e5c80547
Yuma Satake <satous.y@icloud.com>
2023-11-03 22:36:21 +0900

    fix: 環境変数整理

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDO62i3eEhRC5kj_UFMEroD-AQlH1-uT6k',
-  authDomain: 'meigakusai2023.firebaseapp.com',
-  projectId: 'meigakusai2023',
-  storageBucket: 'meigakusai2023.appspot.com',
-  messagingSenderId: '334877287249',
-  appId: '1:334877287249:web:4f4bdfcc17ba6260902970',
-  measurementId: 'G-R32HCRJSJR',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
